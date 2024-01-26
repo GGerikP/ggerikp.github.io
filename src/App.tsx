1df2a5b7dfc2989df892649a4016e323d4878b5e
Gerik Peterson <ggerikp1@gmail.com>
2024-01-26 17:09:42 -0600

    Updating the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import NavigationBar from './components/NavigationBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/TerminalHomePage';
 import 'semantic-ui-css/semantic.min.css';
 import styled from 'styled-components';
 import theme from './theme';
 import Error404NotFoundPage from './pages/Error404NotFoundPage';
-import AboutPage from './pages/AboutPage';
+/*import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
 import EngineeringPage from './pages/EngineeringPage';
+import HomePage from './pages/TerminalHomePage';
 import ManagerPage from './pages/Manager';
 import PoliticsPage from './pages/Politics';
 import TravelerPage from './pages/Traveler';
 import SingerPage from './pages/Singer';
-import PianistPage from './pages/Pianist';
+import PianistPage from './pages/Pianist';*/
 import SPA from './pages/SPA';
 
 const StyledApp = styled.div`
@@ -31,7 +31,7 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/" element={<SPA />} />
-          <Route path="/homepage" element={<HomePage />} />
+          {/*<Route path="/homepage" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/contact" element={<ContactPage />} />
@@ -40,7 +40,7 @@ function App() {
           <Route path="/political-whatifs" element={<PoliticsPage />} />
           <Route path="/traveler" element={<TravelerPage />} />
           <Route path="/singer" element={<SingerPage />} />
-          <Route path="/pianist" element={<PianistPage />} />
+  <Route path="/pianist" element={<PianistPage />} />*/}
           <Route path="*" element={<Error404NotFoundPage />} />
         </Routes>
       </Router>
